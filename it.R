243276840f025d45fa64ee86b1f4819ae530eb62
Peter Burkholder <pburkholder@pobox.com>
2016-04-23 17:51:34 -0400

    Almost

--- it.R
+++ it.R
@@ -8,15 +8,25 @@ exc <- c(38,39,rep(40,3),46,47,49,51,53)
 
 all <- c(cam,cse,se,tr,exc)
 
-gt40 <- function(x) {
-	return (x > 40)
+sample_is_under_40 <- function(x,size=7,youngsters=4) {
+	y <- sample(x, size)  # take sample
+	z <- sum(y < 40)	  # count elements under 40
+	res <- (z >= youngsters)   # True if sample is mostly under 40.
+  res
 }
 
-o40 <- function(x,size,n=4) {
-	y <- sample(x, size)
-	message(y)
-	z <- sum(y > 40)	
+x <- c(TRUE)
+
+for (n in 0:10){
+  a <- sample_is_under_40(all)
+  x <- append(x,a)
 }
 
-	
+loopit <- function(n=10) {
+  for (i in 0:n){
+    a <- sample_is_under_40(all)
+    x <- append(x,a)
+  }
+  x
+}
 
